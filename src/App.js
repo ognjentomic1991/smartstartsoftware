b187d2893b4b70073c5ab60315f51347dca7a770
Ognjen Tomic <ognjentomic1991@gmail.com>
2025-03-26 13:27:34 +0100

    proba2

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Uspeo sam
+          Uspeo sam!!!
         </p>
         <a
           className="App-link"
